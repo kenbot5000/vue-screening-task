bafd5e57479e22143eb9e278e43e3a7a8c3bb366
Rionn Kenshin Pineda <rionnken@outlook.com>
2020-12-07 18:00:32 +0800

    Add CORS

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
-
 const express = require('express');
 const consola = require('consola');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const host = process.env.HOST;
 const port = process.env.PORT;
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:8080'
+}))
+
 const routes = require('./routes');
 app.use('/', routes);
 
