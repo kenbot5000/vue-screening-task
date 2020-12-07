13aefa41cd35cc2d6c26df6cf175e02708de7ee9
Rionn Kenshin Pineda <rionnken@outlook.com>
2020-12-07 18:06:21 +0800

    Basic data view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app app>
+  <v-container>
+    <ItemLoader :dialog-data="dialogData" />
+    <v-row>
+      <v-col>
+        <v-card outlined class="pa-4 primary white--text">
+          <v-card-title class="text-h4">
+            Values
+          </v-card-title>
+          <ItemDisplay v-for="item in values" :key="item._id" :item-data="item.title" @click="loadInfo(item)" />
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card outlined class="pa-4 secondary white--text">
+          <v-card-title class="text-h4">
+            Principles
+          </v-card-title>
+          <ItemDisplay v-for="item in principles" :key="item._id" :item-data="item.title" @click="loadInfo(item)"/>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import ItemDisplay from '@/components/ItemDisplay.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ItemDisplay,
+  },
+  data () {
+    return {
+      values: [],
+      principles: [],
+      dialogData: null,
+    }
+  },
+  mounted () {
+      this.getAllItems()
+  },
+  methods: {
+    async getAllItems () {
+      let items = await axios.get('/api/');
+      items = items.data.res;
+      [this.values, this.principles] = items.reduce((result, element) => {
+        result[element.type === 'value' ? 0 : 1].push(element);
+        return result;
+      },
+      [[],[]]);
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ItemDisplay.vue
@@ -0,0 +1,18 @@
+<template >
+  <v-hover v-slot="{ hover }">
+    <v-card class="pa-4 my-3" :elevation="hover ? 12 : 2" :class="{ 'on-hover' : 'hover'}" link >
+      {{ title }}
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  props: ['item-data'],
+  data () {
+    return {
+      title: this.itemData
+    }
+  }
+}
+</script>
